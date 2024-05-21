36ca3da84b0d062a5d15f215b3fe2b39991acb3a
Raphael Silva <raphel.silva@inventsoftware.com.br>
2024-05-21 16:44:54 -0300

    refatoracao

--- python/pokeapi/pokeapi/urls.py
+++ python/pokeapi/pokeapi/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from myapp.views import MyModelViewSet
+from myapp.views.mymodelviewset import MyModelViewSet
 
 router = DefaultRouter()
 router.register(r'mymodel', MyModelViewSet)
