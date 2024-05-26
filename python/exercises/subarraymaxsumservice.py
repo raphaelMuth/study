08c0553422f23917bf52155291ca76800380de3d
Raphael Silva <raphel.silva@inventsoftware.com.br>
2024-05-26 09:37:33 -0300

    added test cases

--- python/exercises/subarraymaxsumservice.py
+++ python/exercises/subarraymaxsumservice.py
@@ -2,18 +2,18 @@ import pprint
 
 class SubArrayMaxSumService:
  
-    def solve(self, arr):
+    def solution_one(self, arr):
         total_len = len(arr)
         actual_size = 1
         my_dict = {}
         while actual_size <= total_len:
-            my_dict[actual_size] = { "elements": [], "max": [], "max_val": -1 }
+            my_dict[actual_size] = { "elements": [], "max": [], "max_val": None }
 
             init_index = 0
             while init_index < total_len:
                 sub_array = arr[init_index:(init_index+actual_size)]
                 sum_sub_array = sum(sub_array)
-                if sum_sub_array > my_dict[actual_size]["max_val"]:
+                if my_dict[actual_size]["max_val"] == None or sum_sub_array > my_dict[actual_size]["max_val"]:
                     my_dict[actual_size]["max_val"] = sum_sub_array
                     my_dict[actual_size]["max"] = sub_array
 
@@ -24,10 +24,21 @@ class SubArrayMaxSumService:
 
         key_with_max_value = max(my_dict, key=lambda x: my_dict[x]['max_val'])
 
-        pp = pprint.PrettyPrinter(indent=4) 
-        pp.pprint(my_dict)
+        # pp = pprint.PrettyPrinter(indent=4) 
+        # pp.pprint(my_dict)
         
         return my_dict[key_with_max_value]['max']
 
-    def say_hello(self, name):
-        return f"Hello, {name}!"
+    def solution_two(self, arr):
+        total_len = len(arr)
+        actual_size = 1
+        max_sum_array = []
+        while actual_size <= total_len:
+            init_index = 0
+            while init_index < total_len:
+                sub_array = arr[init_index:(init_index+actual_size)]
+                if len(max_sum_array) == 0 or sum(sub_array) > sum(max_sum_array):
+                    max_sum_array = sub_array 
+                init_index = init_index + 1
+            actual_size = actual_size + 1
+        return max_sum_array
--- python/exercises/subarraymaxsumservicetests.py
+++ python/exercises/subarraymaxsumservicetests.py
@@ -1,20 +1,32 @@
 import unittest
+from parameterized import parameterized
 from subarraymaxsumservice import SubArrayMaxSumService
 
 class SubArrayMaxSumServiceTest(unittest.TestCase):
     def setUp(self):
-        self.service = SubArrayMaxSumService()
+        self.service = SubArrayMaxSumService() 
 
-    def test_say_hello(self):
-        self.assertEqual(self.service.say_hello("GitHub Copilot"), "Hello, GitHub Copilot!")
+    @parameterized.expand([
+        ([-1, -2, -3, -4, -5], [-1]),
+        ([1, 2, 3], [1, 2, 3]),
+        ([-2, 1, -3, 4, -1, 2, 1, -5, 4], [4, -1, 2, 1]),
+        ([-2, -3, 4, -1, -2, 1, 5, -3], [4, -1, -2, 1, 5]),
+        ([5, 4, 1, 7, 8], [5, 4, 1, 7, 8])
+    ])
+    def test_must_solve_with_solution_one(self, input_array, expected_output):
+        value = self.service.solution_one(input_array)
+        self.assertEqual(value, expected_output)
 
-    def test_say_helloa(self):
-        value = self.service.solve([1,2,3])
-        self.assertEqual(value, [1,2,3])
-
-    def test_say_helloaaa(self): 
-        value = self.service.solve([-2, 1, -3, 4, -1, 2, 1, -5, 4])
-        self.assertEqual(value, [4, -1, 2, 1])
+    @parameterized.expand([
+        ([-1, -2, -3, -4, -5], [-1]),
+        ([1, 2, 3], [1, 2, 3]),
+        ([-2, 1, -3, 4, -1, 2, 1, -5, 4], [4, -1, 2, 1]),
+        ([-2, -3, 4, -1, -2, 1, 5, -3], [4, -1, -2, 1, 5]),
+        ([5, 4, 1, 7, 8], [5, 4, 1, 7, 8])
+    ])
+    def test_must_solve_with_solution_two(self, input_array, expected_output):
+        value = self.service.solution_two(input_array)
+        self.assertEqual(value, expected_output)
 
 if __name__ == '__main__':
     unittest.main()
