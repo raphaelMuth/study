86fcfca2f6a948547e6def6746f53520091ba83d
Raphael Silva <raphel.silva@inventsoftware.com.br>
2024-05-26 10:38:05 -0300

    third solution create, doing a pre analises

--- python/exercises/subarraymaxsumservice.py
+++ python/exercises/subarraymaxsumservice.py
@@ -42,3 +42,44 @@ class SubArrayMaxSumService:
                 init_index = init_index + 1
             actual_size = actual_size + 1
         return max_sum_array
+
+    def solution_three(self, arr):
+
+        all_negative = True
+        higher_negative = None
+        all_positive = True
+        positives_arr = []
+        first_positive_index = -1
+        index = 0
+        for item in arr:
+            if item < 0:
+                all_positive = False
+                if higher_negative == None or higher_negative < item:
+                    higher_negative = item
+            if item >= 0:
+                all_negative = False
+                positives_arr.append(item)
+                if first_positive_index == -1:
+                    first_positive_index = index
+            index = index + 1
+
+        if all_negative:
+            return [higher_negative]
+        if all_positive:
+            return arr
+        if len(positives_arr) == 1:
+            return positives_arr
+        
+        first_positive_index = first_positive_index if first_positive_index != -1 else 0
+        total_len = len(arr)
+        actual_size = 1
+        max_sum_array = []
+        while actual_size <= total_len:
+            init_index = first_positive_index
+            while init_index < total_len:
+                sub_array = arr[init_index:(init_index+actual_size)]
+                if len(max_sum_array) == 0 or sum(sub_array) > sum(max_sum_array):
+                    max_sum_array = sub_array 
+                init_index = init_index + 1
+            actual_size = actual_size + 1
+        return max_sum_array
--- python/exercises/subarraymaxsumservicetests.py
+++ python/exercises/subarraymaxsumservicetests.py
@@ -28,5 +28,16 @@ class SubArrayMaxSumServiceTest(unittest.TestCase):
         value = self.service.solution_two(input_array)
         self.assertEqual(value, expected_output)
 
+    @parameterized.expand([
+        ([-1, -2, -3, -4, -5], [-1]),
+        ([1, 2, 3], [1, 2, 3]),
+        ([-2, 1, -3, 4, -1, 2, 1, -5, 4], [4, -1, 2, 1]),
+        ([-2, -3, 4, -1, -2, 1, 5, -3], [4, -1, -2, 1, 5]),
+        ([5, 4, 1, 7, 8], [5, 4, 1, 7, 8])
+    ])
+    def test_must_solve_with_solution_three(self, input_array, expected_output):
+        value = self.service.solution_three(input_array)
+        self.assertEqual(value, expected_output)
+
 if __name__ == '__main__':
     unittest.main()
